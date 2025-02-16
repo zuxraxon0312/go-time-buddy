aee797fe72db854f82d645df347225fe485d2b68
Nick Kosarev <hmbanan666@hotmail.com>
2025-02-16 16:25:11 +0200

    feat: website deploy (#302)

--- apps/website/app/components/GithubButton.vue
+++ apps/website/app/components/GithubButton.vue
@@ -2,12 +2,12 @@
   <UTooltip text="GitHub">
     <ULink to="https://github.com/next-orders/food" target="_blank">
       <UButton
-        leading-icon="pixel:github"
+        leading-icon="food:github"
         aria-label="GitHub"
         variant="ghost"
         color="primary"
         size="xl"
-        class="px-2 py-2 gap-1"
+        class="px-2 py-2 gap-2"
       >
         31
       </UButton>
